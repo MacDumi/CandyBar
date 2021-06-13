a44f6b7c9da395f37b33af6f0a0ee18bd0d7b755
MacDumi <me@dumitruduca.com>
2021-06-13 15:26:46 +0200

    Minor changes to teh python setup script

--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from pybind11 import get_cmake_dir
 
-__version__ = "1.0"
+__version__ = "1.2.1"
 
 
 ext_modules = [
-    Pybind11Extension("candybar",
+    Pybind11Extension("candy_bar",
         ["src/pysetup.cpp", "src/candybar.cpp"],
         define_macros = [('VERSION_INFO', __version__)],
         ),
 ]
 
 setup(
-    name="candybar",
+    name="candy_bar",
     version=__version__,
     author="Dumitru Duca",
     author_email="me@dumitruduca.com",
     url="https://github.com/MacDumi/CandyBar",
-    download_url="https://github.com/MacDumi/CandyBar/archive/refs/tags/1.0.tar.gz",
+    download_url="https://github.com/MacDumi/CandyBar/archive/refs/tags/1.2.1.tar.gz",
     description="A simple progress bar with the ILoveCandy style",
     keywords = ['Progress Bar', 'ILoveCandy', 'Pacman'],
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
+    install_requires=['pybind11'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
